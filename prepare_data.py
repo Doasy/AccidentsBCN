42a74fc3e99a4e811914b2a7bf0c074507b6f7fa
Alba Lamas <alba.v.lamas@gmail.com>
2019-04-19 20:01:35 +0200

    update script

--- prepare_data.py
+++ prepare_data.py
@@ -17,13 +17,11 @@ def create_folders():
         os.mkdir('data/vehicles')
 
 def copyfiles(src_path):
-    for year in range(2010,2019):
-        
-        if os.path.exists(src_path):
+    if os.path.exists(src_path):
+        for year in range(2010,2019):
             if os.path.exists(src_path+'/'+str(year)+'_ACCIDENTS_GU_BCN_'+str(year)+'.csv'):
                 origin_accidents = src_path+'/'+str(year)+'_ACCIDENTS_GU_BCN_'+str(year)+'.csv'
             else:
-                print('nop')
                 origin_accidents =  src_path+'/'+str(year)+'_accidents_gu_bcn'+'.csv'       
         
             if os.path.exists(src_path+'/'+str(year)+'_ACCIDENTS_CAUSES_GU_BCN_'+str(year)+'.csv'):
@@ -59,9 +57,16 @@ def copyfiles(src_path):
             copyfile(origin_persones, destination_persones)
             copyfile(origin_tipus, destination_tipus)
             copyfile(origin_vehicles, destination_vehicles)
-    
+    else:
+        print("This path doesn't exist.") 
+        exit(1)
+
+
 
 if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("It's necessary only one argument: the path where the download data is.") 
+        exit(1)
     folder_path = sys.argv[1]
     create_folders()
     copyfiles(folder_path)
