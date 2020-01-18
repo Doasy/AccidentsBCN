676df961c14f950a2e01a6c7d27d08d175b642c0
marc <mariva1995@gmail.com>
2020-01-18 16:15:44 +0100

    one of the final generations is done

--- scripts/build_features.py
+++ scripts/build_features.py
@@ -1,21 +1,61 @@
 import pandas as pd
+import os
 
 
+def expertise_definer(row):
+    if row < 2:
+        return 0
+    elif row < 5:
+        return 1
+    elif row < 10:
+        return 2
+    else:
+        return 3
 
-# edat del vehicle
 
-# marca del vehicle
+def extract_vehicles_info(df, vehicles):
+    df = df.merge(vehicles, how='inner', on="Número d'expedient")
+    df.rename({'Any_x': 'any', "Descripció tipus de vehicle": 'tipus_vehicle', 'Descripció model': 'model',
+               'Descripció marca': 'marca', "Descripció color": 'color_vehicle', 'Descripció carnet': 'tipus_carnet',
+               'Antiguitat carnet': 'antiguitat_carnet'}, axis=1, inplace=True)
+    df.drop(df.loc[df.antiguitat_carnet == 'Desconegut'].index, axis=0, inplace=True)
+    df.antiguitat_carnet = df.antiguitat_carnet.astype('int')
+    df['expertesa_carnet'] = df.antiguitat_carnet.apply(expertise_definer)
+    df.drop(['Any_y', 'antiguitat_carnet'], axis=1, inplace=True)
+    return df
 
-# color del vehicle
-
-# tipus de vehicle
-
-# antiguetat de llicencia de conduccio
-
-# tipus de llicencia
 
 # rangs de edats dels conductors
-
 # genere del conductor
+def driver_info(df, driver):
+    driver.drop(driver.loc[driver['Descripció tipus persona'] != 'Conductor'].index, axis=0, inplace=True)
+    driver = driver[['Descripció sexe', 'Edat']]
+    df = df.merge(driver, how='inner', on="Número d'expedient")
+    df.rename({'Any_x': 'any', 'Descripció sexe': 'genere', 'Edat': 'edat'}, axis=1, inplace=True)
+    df.drop(df.loc[df.edat == 'Desconegut'].index, axis=0, inplace=True)
+    df.edat = df.edat.astype('int')
+    df['rang_edat'] = df.edat.apply(lambda x: 0 if x < 30 else 1)
+    df['rang_edat'] = df.edat.apply(lambda x: 1 if x < 65 else 2)
+    df.drop('edat', axis=1, inplace=True)
+    return df
+
 
 # nombre d'accidents donat un perfil
+def accidents_number(df):
+    print(df.groupby(['any', 'tipus_vehicle', 'model', 'marca', 'color_vehicle',
+       'tipus_carnet', 'expertesa_carnet', 'genere', 'rang_edat']).size())
+    # no contar accidents, sino fer un factor de risc. Aplicar factor de risc a differents agrupacions
+    return df
+
+
+if __name__ == '__main__':
+    if not os.path.exists('../data/final'):
+        os.mkdir('../data/final')
+    accidents = pd.read_csv('../data/merged/accidents.csv', index_col="Número d'expedient")
+    vehicles = pd.read_csv('../data/merged/vehicles.csv', index_col="Número d'expedient")
+    persones = pd.read_csv('../data/merged/persones.csv', index_col="Número d'expedient")
+    df = pd.DataFrame(accidents["Any"])
+    (df.pipe(extract_vehicles_info, vehicles)
+     .pipe(driver_info, persones)
+     .pipe(accidents_number)
+     .to_csv('../data/final/featured.csv'))
--- scripts/merge_data.py
+++ scripts/merge_data.py
@@ -5,37 +5,37 @@ from functools import reduce
 
 
 def create_folders():
-    if not os.path.exists('../data/final'):
-        os.mkdir('../data/final')
+    if not os.path.exists('../data/merged'):
+        os.mkdir('../data/merged')
+
 
 def merge_data(list_of_files):
     for i in range(9):
-        list_of_files[i]['Any'] = 2010+i        
-    return reduce(lambda  left,right:  pd.concat([left,right], join="inner"), list_of_files)
+        list_of_files[i]['Any'] = 2010 + i
+    return reduce(lambda left, right: pd.concat([left, right], join="inner"), list_of_files)
 
 
 def get_data():
-    accidents = [pd.read_csv('../data/processed/'+str(2010+i)+'/accidents.csv') for i in range(9)]
-    causes = [pd.read_csv('../data/processed/'+str(2010+i)+'/causes.csv') for i in range(9)]
-    domicilis_sexe = [pd.read_csv('../data/processed/'+str(2010+i)+'/domicilis_sexe.csv') for i in range(9)]
-    padro_ocupacio = [pd.read_csv('../data/processed/'+str(2010+i)+'/padro_ocupacio.csv') for i in range(9)]
-    persones = [pd.read_csv('../data/processed/'+str(2010+i)+'/persones.csv') for i in range(9)]
-    tipus = [pd.read_csv('../data/processed/'+str(2010+i)+'/tipus.csv') for i in range(9)]
-    vehicles = [pd.read_csv('../data/processed/'+str(2010+i)+'/vehicles.csv') for i in range(9)]
+    accidents = [pd.read_csv('../data/processed/' + str(2010 + i) + '/accidents.csv') for i in range(9)]
+    causes = [pd.read_csv('../data/processed/' + str(2010 + i) + '/causes.csv') for i in range(9)]
+    domicilis_sexe = [pd.read_csv('../data/processed/' + str(2010 + i) + '/domicilis_sexe.csv') for i in range(9)]
+    padro_ocupacio = [pd.read_csv('../data/processed/' + str(2010 + i) + '/padro_ocupacio.csv') for i in range(9)]
+    persones = [pd.read_csv('../data/processed/' + str(2010 + i) + '/persones.csv') for i in range(9)]
+    tipus = [pd.read_csv('../data/processed/' + str(2010 + i) + '/tipus.csv') for i in range(9)]
+    vehicles = [pd.read_csv('../data/processed/' + str(2010 + i) + '/vehicles.csv') for i in range(9)]
 
     return accidents, causes, domicilis_sexe, padro_ocupacio, persones, tipus, vehicles
 
+
 if __name__ == "__main__":
     create_folders()
-    accidents, causes, domicilis_sexe, padro_ocupacio, persones, tipus,  vehicles = get_data()
-    merge_data(accidents).to_csv('../data/final/accidents.csv', index = False)
-    merge_data(causes).to_csv('../data/final/causes.csv', index = False)
-    merge_data(domicilis_sexe).to_csv('../data/final/domicilis_sexe.csv', index = False)
-    merge_data(padro_ocupacio).to_csv('../data/final/padro_ocupacio.csv', index = False)
-    merge_data(persones).to_csv('../data/final/persones.csv', index = False)
-    merge_data(tipus).to_csv('../data/final/tipus.csv', index = False)
-    merge_data(vehicles).to_csv('../data/final/vehicles.csv', index = False)
-    districtes_i_barris = pd.read_csv('../data/processed/districtes_i_barris.csv')
-    districtes_i_barris.to_csv('../data/final/districtes_i_barris.csv', index = False)
-
-
+    accidents, causes, domicilis_sexe, padro_ocupacio, persones, tipus, vehicles = get_data()
+    merge_data(accidents).to_csv('../data/merged/accidents.csv', index=False)
+    merge_data(causes).to_csv('../data/merged/causes.csv', index=False)
+    merge_data(domicilis_sexe).to_csv('../data/merged/domicilis_sexe.csv', index=False)
+    merge_data(padro_ocupacio).to_csv('../data/merged/padro_ocupacio.csv', index=False)
+    merge_data(persones).to_csv('../data/merged/persones.csv', index=False)
+    merge_data(tipus).to_csv('../data/merged/tipus.csv', index=False)
+    merge_data(vehicles).to_csv('../data/merged/vehicles.csv', index=False)
+    pd.read_csv('../data/processed/districtes_i_barris.csv').to_csv('../data/merged/districtes_i_barris.csv',
+                                                                    index=False)
